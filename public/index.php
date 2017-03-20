c4364ef883a1d79927149c3e55d7c4a83fe89b98
艺品网络 <593657688@qq.com>
2017-03-20 02:24:44 +0000

    no commit message

--- public/index.php
+++ public/index.php
@@ -20,8 +20,8 @@ define('APP_PATH', __DIR__ . '/../application/');
 define('NOW_TIME',      $_SERVER['REQUEST_TIME']);
 //是否安装
 if(!is_file(APP_PATH . '/database.php')){
-    $module_name = \think\Request::instance()->module();
-	header('Location: ./install.php/install/index/'.$module_name);exit();
+  return  $module_name = \think\Request::instance()->module();
+//	header('Location: ./install.php/install/index/'.$module_name);exit();
 }
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
